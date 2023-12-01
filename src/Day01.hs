d366709a84fa3f6458e269af9bbab1ef4c3148b5
Olle Linderos <olle@linderos.nu>
2023-12-01 22:24:40 +0100

    Day01 done

---
+++ src/Day01.hs
@@ -0,0 +1,48 @@
+module Day01 where
+
+import Data.Char (isDigit)
+
+-- Part 1
+
+processLines :: [String] -> Int
+processLines x = result
+ where
+  result = sum $ map (\y -> read [head y, last y]) digits
+  digits = map (filter isDigit) x
+
+-- Part 2
+
+digitizeWords :: String -> String
+digitizeWords x
+  | "one"   == take 3 x = '1' : tail x
+  | "two"   == take 3 x = '2' : tail x
+  | "three" == take 5 x = '3' : tail x
+  | "four"  == take 4 x = '4' : tail x
+  | "five"  == take 4 x = '5' : tail x
+  | "six"   == take 3 x = '6' : tail x
+  | "seven" == take 5 x = '7' : tail x
+  | "eight" == take 5 x = '8' : tail x
+  | "nine"  == take 4 x = '9' : tail x
+  | otherwise = x
+
+digitize :: String -> String -> String
+digitize x "" = x
+digitize x y = digitize (x ++ f) l
+ where
+  (f, l) = splitAt 1 $ digitizeWords y
+
+processLines' :: [String] -> Int
+processLines' x = result
+ where
+  result = sum $ map (\y -> read [head y, last y]) digits
+  digits = map (filter isDigit . digitize "") x
+
+day01 :: IO ()
+day01 = do
+  inputLines <- lines <$> readFile "data/day01.txt"
+
+  let p1 = processLines inputLines
+  let p2 = processLines' inputLines
+
+  putStrLn $ "Part 1: " ++ show p1
+  putStrLn $ "Part 2: " ++ show p2
